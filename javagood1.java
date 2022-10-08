ebb0db317a3e1d38bd85db47cccb15f2a5d5b37c
leblos <99028801+leblos@users.noreply.github.com>
2022-10-08 23:00:16 +0800

    Add files via upload

---
+++ javagood1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+        //有一个数组 {1， 2， 3， 4， 5}， 可以将该数组进行缩减，提示用户是否继续缩减，
+        // 每次缩减最后那个元素。当只剩 下最后一个元素，提示，不能再缩减。
+        do {
+            int n = 4;
+            int arr[] = {1, 2, 3, 4, 5};
+            int arr1[] = new int[n];
+
+            for (int i = 0; i < arr1.length; i++) {
+                arr1[i] = arr[i];
+            }
+            arr = arr1;
+
+            for (int j = 0; j < arr.length; j++) {
+                System.out.println(arr[j]);
+            }
+                Scanner myScanner = new Scanner(System.in);
+                System.out.println("是否继续缩减Y/N");
+                Char m = myScanner.next().charAt(0);
+
+                if (m=="Y"||n>1){
+                    n--;
+                }else{
+                    System.out.println("只剩下最后一个元素");
+                    break;
+                }
+
+
+
+            }while(true);
+        }
+    }
